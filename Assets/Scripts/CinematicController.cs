e1b43feb8445ce0c268222a8a4ca16959f2ff4be
Jeremy Mamani <jeremymamani832@gmail.com>
2026-04-14 10:01:44 -0500

    Changes

---
+++ Assets/Scripts/CinematicController.cs
@@ -0,0 +1,13 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class CinematicController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+}
